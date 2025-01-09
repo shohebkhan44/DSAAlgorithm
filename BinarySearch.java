866e14992b187e292b852f210ed03d0973e237c3
shohebkhan44 <shohebkhan14916@gmail.com>
2025-01-09 13:19:19 +0530

    new changes added 9/1/24

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,22 +3,27 @@ public class BinarySearch {
     
 
 public static int BinarySearch(int target,int[] arr){
-int size=arr.length;
 int low=0;
+int size=arr.length;
 int high=size-1;
-  while (low<=high) {
 
-    int mid = (low + high ) / 2;
-    if(arr[mid]==target)
-    {
-        return mid;
-    }
-    else if(target>arr[mid]){
-        low=mid+1;
-    }
-    else{
-        high=mid-1;
-    }
+
+  while (low<=high) 
+  {
+    int mid=(low+high)/2;
+     if(arr[mid]==target)
+     {
+      return mid;
+     }
+     else if(target<arr[mid])
+     {
+          high=mid-1;
+     }
+     else
+     {
+      low=mid+1;
+     }
+  
   }
   return -1;
 }
@@ -26,10 +31,10 @@ int high=size-1;
 
 public static void main(String[] args) {
     int array[]={1,3,5,6,7,9,12,79};
-    int target=9;
+      int target=9;
     int ans=BinarySearch(target,array);
 
-    System.out.println("the elemnt found at index  "+ans);
+    System.out.println("the elemnt found at index"+ ans);
     
     
 }
