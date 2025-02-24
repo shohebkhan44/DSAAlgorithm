a2508b71568bce6fbefd055b215004c699d3f646
shohebkhan44 <shohebkhan14916@gmail.com>
2025-02-24 12:00:01 +0530

    new changes added 2/24

--- Main.java
+++ Main.java
@@ -33,19 +33,20 @@ System.out.println("tayhe new arr"+Arrays.toString(arr));
     public static void main(String[] args) {
       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
       int ages[]={13,5,6,8,9,5};
-      int ar[]={1,2,3,6,8};
-      int ar2[]={4,5,6,7,8};
+     String[] Logos={"audi","BMW","bentley"};
       int lowest=ages[0];
-      duplicate(ar, ar2);
-      Secondsmallest(ages);
-      reverseInplace(ar);
-
+      //duplicate(ar, ar2);
+      //Secondsmallest(ages);
+      //reverseInplace(ar);
+ for(String i:Logos){
+  System.out.println(i);
+ }
       for(int i:ages){
         if (lowest> i) {
           lowest=i;
         }
       }
-      System.out.println("the lowest age is" +  lowest);
+     //System.out.println("the lowest age is" +  lowest);
       //System.out.println(cars[2]);
       //System.out.println(cars.length);
 
