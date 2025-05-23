512167157437b6a3923f4397fb104bc825f5c086
shohebkhan44 <shohebkhan14916@gmail.com>
2025-05-23 13:10:00 +0530

    new changes added 23-5

--- ArrayDSA.java
+++ ArrayDSA.java
@@ -77,15 +77,36 @@ for(int i=arr.length-position;i<arr.length;i++)
 return arr;
 
 }
+//Shift zeroes to end
+static int[] shiftZeroes(int[] arr){
+int j=0;
+    for(int i=0;i<arr.length;i++){
+          if(arr[i]!=0){
+            arr[j]=arr[i];
+            j++;
+          }
+    }
+    for(int k=j;k<arr.length;k++){
+        arr[k]=0;
+    }
+    return arr;
+}
+
 
     public static void main(String[] args) {
         int[] ar={1,3,5,7,8};
         int[] ar2={1,1,2,2,2,2,3,4,5,9,7,13};
+        int [] ar3={1,4,0,5,4,9,12,0};
         // Set<Integer> st=new HashSet<>();
         // for(int i=0;i<ar2.length;i++){
         //     //to insert into a set TC=nlogn
         //     st.add(ar2[i]);
         // }
+        System.out.println("arrays wth xeroes at end");
+        
+        for (int i : shiftZeroes(ar3)) {
+            System.out.print(" " +i);
+        }
         System.out.println("left rotate d positions");
         int[] rotateDpos=rotateDpositions(ar, 3);
         for (int i : rotateDpos) {
