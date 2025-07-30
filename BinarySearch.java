2ae928887d9a15ae003cc03a0bacb788765360aa
shohebkhan44 <shohebkhan14916@gmail.com>
2025-07-30 21:29:13 +0530

    new changes added 7-30

--- BinarySearch.java
+++ BinarySearch.java
@@ -88,38 +88,85 @@ else{
 }
 }
 //find Floor and ciel
-public static void Floor(int[] arr){
+public static int Floor(int[] arr,int x){
 
   int low=0;int high=arr.length-1;
   int ans=-1;
-int result[];
+  int result[];
   while(low<=high){
 
     int mid=(low+high)/2;
 
-    if(arr[mid]>=mid){
+    if(arr[mid]<=x){
       ans=arr[mid];
-      
+     
+       low=mid+1;
     }
     else{
+      high=mid-1;
 
     }
   }
+  return ans;
 }
 
+
+//Search in rotated sorted array
+
+public static int SearchRotated(int[] arr,int target)
+{
+    int low=0;
+    int high=arr.length-1;
+    int index=-1;
+int mid=-1;;
+    while (low<=high) {
+      
+   mid=(low+high)/2;
+   if(arr[mid]==target){
+     return mid;
+   }
+   //chk left sorted
+   else if(arr[low]<=arr[mid]){
+    if(arr[low]<=target && target<=arr[mid]){
+
+      high=mid-1;
+    }
+   
+   else{
+    low=mid+1;
+   }
+  }
+
+    else{
+          if(arr[mid]<=target && target<=arr[high]){
+            low=mid+1;
+          }
+          else{
+            high=mid-1;
+          }
+         }  
+    }
+    return -1;
+
+
+}
 public static void main(String[] args) {
-    int array[]={1,3,5,6,7,8,9};
+    int array[]={7,8,9,1,2,3,4,5,6};
       int target=79;
 
     int ans=BinarySearch(target,array);
-   System.out.println("ur element");
-    System.out.println("the elemnt found at index"+ ans);
-    //System.out.println(FirtAndLastOccurence(array,8));
-    System.out.println(LowerBound(array, 8));
-    System.out.println(UpperBound(array, 8));
-    for (int i : FirtAndLastOccurence(array,8)) {
-      System.out.println(i);
-    }
+  //  System.out.println("ur element");
+  //   System.out.println("the elemnt found at index"+ ans);
+  //   //System.out.println(FirtAndLastOccurence(array,8));
+  //   System.out.println(LowerBound(array, 8));
+  //   System.out.println(UpperBound(array, 8));
+   
+  //   for (int i : FirtAndLastOccurence(array,9)) {
+  //     System.out.println(i);
+  //   }
+   // System.out.println("The floor from the given array is"+ Floor(array,9));
+   System.out.print("index at");
+   System.out.println(SearchRotated(array, 6));
 
 
 
