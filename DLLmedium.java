60efbc842e50dfe5b2303cdf0287a254264b3192
shohebkhan44 <shohebkhan14916@gmail.com>
2025-09-11 20:58:30 +0530

    new changes added 9-11

--- DLLmedium.java
+++ DLLmedium.java
@@ -124,7 +124,7 @@ public static List<List<Integer>> Findpairs(Node head, int sum) {
     public static void main(String[] args) {
 
 
-        int[] arr={1,2,3,4,5,6,7,8,9};
+        int[] arr={1,2,3,4,5,6,7,8,9,12};
         int targetSum=8;
           Node head=ConvertToDLL(arr);
         // head=DeleteOccurrences(head,4);
