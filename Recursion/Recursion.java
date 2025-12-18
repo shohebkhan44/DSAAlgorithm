1e114853c1c40b32efe1b14d496997c2766b6717
shohebkhan44 <shohebkhan14916@gmail.com>
2025-12-18 11:43:44 +0530

    18-12-2025

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -4,7 +4,11 @@ public class Recursion {
     
     public static void main(String[] args) {
         // Print(5);
-        System.out.println( Fibonacci(9));
+       // System.out.println( Fibonacci(3));
+       int[] arr={2,3,4,5};
+       int high=arr.length-1;
+       System.out.println("Tartget value"+BinarySearch(arr,0,high,4));
+
     }
 
     private static void Print(int n){
@@ -17,5 +21,20 @@ public class Recursion {
          if (n == 0) return 0;   // base case
         if (n == 1) return 1;   // base case
         return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+    private static int BinarySearch(int arr[],int low,int high,int target){
+ if(low>high) return -1;
+        int mid=(low+high)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]>target){ 
+               high=mid-1;
+           return BinarySearch(arr, low, high, target);
+        }
+        else{
+            low=mid+1;
+          return  BinarySearch(arr, low, high, target); 
+        }
+    
+ 
     }
 }
